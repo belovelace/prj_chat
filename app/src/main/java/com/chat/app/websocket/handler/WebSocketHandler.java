3647e6ad9ed28e3a51377836362d28c00895cb5d
Eun Ji Shin <159417728+belovelace@users.noreply.github.com>
2025-03-05 14:48:51 +0900

    소켓통신 log 삽입

--- app/src/main/java/com/chat/app/websocket/handler/WebSocketHandler.java
+++ app/src/main/java/com/chat/app/websocket/handler/WebSocketHandler.java
@@ -1,6 +1,9 @@
 package com.chat.app.websocket.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -18,6 +21,9 @@ import java.util.Set;
  * 소켓통신 특성상 1:n 관계. 즉, 한 서버에 여러 클라이언트 접속 가능
  */
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -29,12 +35,14 @@ public class WebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessions.add(session); //새로운 세션 추가(사용자 접속)
+        log.info("[새로운 연결]New connection established: {}", session.getId());
     }
 
     //소켓 연결 종료
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         sessions.remove(session);//세션 제거(사용자 연결 종료)
+        log.info("[연결 종료]", session.getId());
     }
 
     //소켓 메세지 처리
@@ -42,6 +50,8 @@ public class WebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         //클라이언트가 보낸 메세지를 역직렬화
         String json = message.getPayload();
+        log.info("[수신 메세지]: {}", json);
+       //역직렬화
     }
 
 
